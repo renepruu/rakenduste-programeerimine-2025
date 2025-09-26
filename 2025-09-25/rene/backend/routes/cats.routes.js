ea6b8568151520d272e148a3687c7df2afc46359
rene <prene@tlu.ee>
2025-09-26 11:55:23 +0300

    fix(cats): add validation for create and update endpoints

--- 2025-09-25/rene/backend/routes/cats.routes.js
+++ 2025-09-25/rene/backend/routes/cats.routes.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const catsController = require("../controllers/cats.controller")
+const { body, validationResult } = require("express-validator")
+
 const {
   catsRouteMiddleware,
   catsGetRouteMiddleware,
@@ -10,8 +12,29 @@ router.use(catsRouteMiddleware)
 
 // /cats/ Get endpoint level middleware
 router.get("/", catsGetRouteMiddleware, catsController.read)
-router.post("/", catsController.create)
-router.put("/", catsController.update)
+router.post(
+  "/",
+  body("name").trim().notEmpty().withMessage("Name is required"),
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() })
+    next()
+  },
+  catsController.create,
+)
+router.put(
+  "/",
+  body("id").trim().notEmpty().withMessage("ID is required"),
+  body("name").trim().notEmpty().withMessage("Name is required"),
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() })
+    next()
+  },
+  catsController.update,
+)
 router.delete("/", catsController.delete)
 
 module.exports = router
